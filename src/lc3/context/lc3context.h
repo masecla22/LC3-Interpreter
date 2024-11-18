b0967bf6df9d7ecb26e016086a387b3676d4a3e6
masecla22 <supermasecla22@gmail.com>
2024-11-18 10:34:25 +0100

    Create lc3context.h

---
+++ src/lc3/context/lc3context.h
@@ -0,0 +1,15 @@
+#ifndef LC3_CONTEXT_H
+#define LC3_CONTEXT_H
+
+#include <stdio.h>
+
+typedef struct LC3Context {
+    FILE* inputFile;
+    FILE* outputFile;
+
+    int randomized;
+    int seed;
+} LC3Context;
+
+
+#endif // LC3_CONTEXT_H
