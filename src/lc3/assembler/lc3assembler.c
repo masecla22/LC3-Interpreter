61584a9e90991a0a02cba2b1b8241f992ecfc7f9
masecla22 <supermasecla22@gmail.com>
2024-11-20 01:50:47 +0100

    Added the list of parsedInstructions inside the assembler

--- src/lc3/assembler/lc3assembler.c
+++ src/lc3/assembler/lc3assembler.c
@@ -1,8 +1,25 @@
 #include "lc3assembler.h"
 
 #include "../../lexer/lexer.h"
+#include "../instructions/lc3isa.h"
+
+#include <stdlib.h>
+
+extern LabelledInstructionList *parsedInstructions;
+
+void freeParsedInstructions() {
+    if (parsedInstructions == NULL) {
+        return;
+    }
+
+    destroyLabelledInstructionList(parsedInstructions);
+    parsedInstructions = NULL;
+}
 
 void assemble(LC3Context ctx) {
+    // Register parser cleanup
+    atexit(freeParsedInstructions);
+
     // Setup the lexer with the input file.
     initLexer(ctx.inputFile);
 
@@ -10,5 +27,8 @@ void assemble(LC3Context ctx) {
     yyparse();
 
     // Free the lexer resources.
-    finalizeLexer();
+    finalizeLexer();    
+
+    // Free the parsed instructions
+    freeParsedInstructions();
 }
